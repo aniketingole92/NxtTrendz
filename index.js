6c6352777d4e712ec0b696906aa844cb500d8aed
Aniket Ingole <148335687+aniketingole92@users.noreply.github.com>
2025-12-20 18:26:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import './index.css'
 
-const Home = () => (
-  <div className="home-container">
+const Contact = () => (
+  <div className="contact-container">
     <img
-      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
-      alt="home"
-      className="home-img"
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      alt="contact"
+      className="contact-img"
     />
-    <h1 className="home-heading">Home</h1>
+    <h1 className="contact-heading">Contact</h1>
+    <p className="contact-paragraph1"> Aniket Ingole </p>
+    <p className="contact-paragraph"> 8530448522</p>
   </div>
 )
 
-export default Home
+export default Contact
